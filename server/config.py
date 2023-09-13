3f71cad958294e7157081636cf901c29c8acf46d
EduRodgz <138835294+EduRodgz@users.noreply.github.com>
2023-09-12 21:50:14 -0500

    Add files via upload

---
+++ server/config.py
@@ -0,0 +1,11 @@
+developer = {
+    "first":"Eduardo",
+    "last":"Rodriguez",
+    "email":"edurodgz.usmc@icloud.com",
+    "hobbies":["gym","video games","code"],
+    "address":{
+        "num":741,
+        "street":"dartmouth ln",
+        "city":"deer park"
+    }
+}
